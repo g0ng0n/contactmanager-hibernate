7f7cfc1385fa24dc6e4de6d77b240e37ad857fa8
Gonzalo Gisbert <gonzalo.a.gisbert@gmail.com>
2017-04-07 23:30:31 -0300

    added the buildSessionFactory method

--- src/main/java/com/g0ng0n/contactmgr/Application.java
+++ src/main/java/com/g0ng0n/contactmgr/Application.java
@@ -1,9 +1,26 @@
 package com.g0ng0n.contactmgr;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.mapping.MetadataSource;
+import org.hibernate.service.ServiceRegistry;
+
 /**
  * Created by gonzalo.gisbert on 07/04/17.
  */
 public class Application {
+    // holds a reusable reference to aSession Factory (since we need only one)
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+
+    private static SessionFactory buildSessionFactory() {
+
+        //create a StandardServiceRegistry
+        final ServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
+        return new MetadataSources(registry).buildMetadata().buildSessionFactory();
+
+    }
 
     public static void main (String[] args){
 
